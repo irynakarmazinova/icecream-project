c53e467ee2c6d57dd5ca27e49fd2fe4f66a91417
Serhii-Zvieriev <zacjok@gmail.com>
2021-04-25 13:35:46 +0300

    Галерея готова на 96%
    осталось добавить webp картинки и секция галерея с доп задание сделана

--- src/js/masonry-layout-gallery.js
+++ src/js/masonry-layout-gallery.js
@@ -1,30 +1,7 @@
-// $(document).ready(function(){ 
-// 	$('.grid').masonry({
-// // указываем элемент-контейнер в котором расположены блоки для динамической верстки
-// 	    itemSelector: '.grid-item',
-// // указываем класс элемента являющегося блоком в нашей сетке
-//         singleMode: false,
-// // перестраивает блоки при изменении размеров окна
-//         isAnimated: true,
-// // анимируем перестроение блоков
-//         animationOptions: { 
-// 	    queue: false, 
-// 	    duration: 500 },
-// // опции анимации - очередь и продолжительность анимации
-//         gutter: 5
-// 	}); 
-//   });
-
-
-// $('.grid').masonry({
-//     itemSelector: '.grid-item',
-//     gutter: 4,
-// });
 
 var $grid = $('.grid').masonry({
     itemSelector: '.grid-item',
     gutter: 4
-//   columnWidth: 160
 });
 
 $grid.on( 'click', '.grid-item', function() {
