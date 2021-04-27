2a0bba63ba6b426f5870e41c8e8ccc8af8ecae5f
irynakarmazinova <i.karmazinova1992@gmail.com>
2021-04-27 18:59:38 +0300

    фиксила ошибку джс

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -11,3 +11,18 @@
     mobileMenuRef.classList.toggle("is-open");
   });
 })();
+
+// (() => {
+//   const refs = {
+//     openModalBtn: document.querySelector('[data-menu-button]'),
+//     closeModalBtn: document.querySelector('[data-menu]'),
+//     modal: document.querySelector('[data-menu-close]'),
+//   };
+
+//   refs.openModalBtn.addEventListener('click', toggleModal);
+//   refs.closeModalBtn.addEventListener('click', toggleModal);
+
+//   function toggleModal() {
+//     refs.modal.classList.toggle('is-hidden');
+//   }
+// })();
